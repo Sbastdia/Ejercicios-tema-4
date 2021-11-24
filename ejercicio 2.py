82df863a52109ebc98bfd24d4b9e663877c6b9e6
Sbastdia <sbastdia@myuax.com>
2021-11-24 10:27:00 +0100

    ejercicio 2

---
+++ ejercicio 2.py
@@ -0,0 +1,7 @@
+def estadoAgua (estadoSolido, estadoLiquido):
+    if(estadoSolido==True):
+        print('El estado del agua es hielo')
+    elif(estadoLiquido==True):
+        print('El estado del agua es líquido')
+    else:
+        print('El estado del agua es vapor')
